d9e760ab74f79587c64df79c4140179a5706cca8
Ananda Prema <77527794+menandap@users.noreply.github.com>
2023-12-07 14:58:30 +0800

    upp

--- framework_files/app_files/main.go
+++ framework_files/app_files/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, World!")
+    })
+
+    fmt.Println("Server started on :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        panic(err)
+    }
 }
